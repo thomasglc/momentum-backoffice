cb5f71775ba51afc5271e5c1aa003f09208d694c
Thomas <thomas.galocha@hotmail.fr>
2026-05-31 20:46:10 +0200

    feat: install @vuepic/vue-datepicker

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import './style.css'
+import '@vuepic/vue-datepicker/dist/main.css'
 import App from './App.vue'
 import router from './router'
 
